92b68dce8f73b1a1e854fcf368003c4661fab2e4
nickmwangemi <nickmwangemi@gmail.com>
2021-04-20 16:10:42 +0300

    Prepare for heroku deploy

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,4 +1,20 @@
+import os
+import dj_database_url
 from .base import *
+
+
+env = os.environ.copy()
+
+
+SECRET_KEY = env['SECRET_KEY']
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+DEBUG =  False
 
 DEBUG = False
 
