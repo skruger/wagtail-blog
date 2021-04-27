fee5d1d9ec03195be20ba13f3ce0fe5da57375c5
nickmwangemi <nickmwangemi@gmail.com>
2021-04-27 20:19:02 +0300

    Add streamfields

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,10 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.embeds.blocks import EmbedBlock
 
 
 class BlogIndexPage(Page):
@@ -33,11 +37,14 @@ class BlogIndexPage(Page):
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
-    body = RichTextField(blank=True)
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title", icon="title")),
+        ('paragraph', blocks.RichTextBlock(icon="pilcrow")),
+        ('embed', EmbedBlock(icon="media")),
+    ])
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
         FieldPanel('intro'),
-        FieldPanel('body', classname="full"),
-
+        StreamFieldPanel('body')
     ]
